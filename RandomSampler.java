d2d3df5ca925d4948944d77d3ff9413a848f3ced
casskycity <castieltong@gmail.com>
2020-06-28 17:00:28 +0800

    improved RandomSampler.java

--- RandomSampler.java
+++ RandomSampler.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 import java.util.List;
 import java.util.Arrays;
+import java.util.HashMap;   
 import java.util.ArrayList;
-import org.apache.commons.lang.ArrayUtils;
 
 public class RandomSampler {
     public static void main(String[] args) {
@@ -11,35 +11,106 @@ public class RandomSampler {
                               "I go hiking by bus.",
                               "I go to sea by bus",
                               "I go to home by bus."};
-        int rand = 3;
 
-        RandSampler RS = new RandSampler(sentences, rand);
-        System.out.println("The random selected sentences are: " + RS.generate());
+        RandSampler RS = new RandSampler(sentences);
+        
+        long startTime = System.nanoTime();
+        for(int i=0; i<100000; i++) {
+            List<String> t = RS.generate(3);
+            t.get(0);
+        }
+        long finishTime = System.nanoTime() - startTime;
+        System.out.println("The time span is " + finishTime);
+        
+        startTime = System.nanoTime();
+        for (int i=0; i<100000; i++) {
+            List<String> t = RS.generatefast(3);
+            t.get(0);
+        }
+        finishTime = System.nanoTime() - startTime;
+        System.out.println("The time span is " + finishTime);
+
+        startTime = System.nanoTime();
+        for (int i=0; i<100000; i++) {
+            List<String> t = RS.generatetuple(3);
+            t.get(0);
+        }
+        finishTime = System.nanoTime() - startTime;
+        System.out.println("The time span is " + finishTime);
     }
 }
 
 
 class RandSampler {
+    private Random random = new Random();
     private String[] sentences;
-    private int rand;
 
-    public RandSampler(String[] sentences, int rand) {
+    public RandSampler(String[] sentences) {
         this.sentences = sentences;
-        this.rand = rand;
     }
 
-    public List<String> generate() {
-        Random random = new Random();
-
-        //create a temporary list for storing selected elements
-        List<String> newList = new ArrayList();
-        List<String> sentList = new ArrayList<String>(Arrays.asList(this.sentences));
-        for (int i = 0; i < this.rand; i++) {
-            int randomIndex = random.nextInt(this.sentences.length);
-            newList.add(sentList.get(randomIndex));
-            //this.sentences = ArrayUtils.remove(this.sentences, randomIndex);
+    public List<String> generate(int num) {
+        List<String> result = new ArrayList<>(num);
+        ArrayList<String> sentList = new ArrayList<>(Arrays.asList(this.sentences));
+        for (int i = 0; i < num; i++) {
+            int randomIndex = this.random.nextInt(sentList.size());
+            result.add(sentList.get(randomIndex));
             sentList.remove(randomIndex);
         }
-        return newList;
+        return result;
+    }
+
+    public List<String> generatefast(int num) {
+        List<String> result = new ArrayList<String>(num);
+
+        HashMap<String, Boolean> items = new HashMap<String, Boolean>();
+
+        for(String s : this.sentences) {
+            items.put(s, false);
+        }
+
+        Object[] index = items.entrySet().toArray();
+
+        while (result.size() < num) {
+            int i = this.random.nextInt(this.sentences.length);
+            HashMap.Entry<String, Boolean> entry = (HashMap.Entry<String, Boolean>) index[i];
+
+            if (!entry.getValue()) {
+                entry.setValue(true);
+                result.add((String) entry.getKey());
+            }
+        }
+
+        return result;
+    }
+
+    public List<String> generatetuple(int num) {
+        List<String> result = new ArrayList<String>(num);
+        Pair[] items = new Pair[this.sentences.length];
+
+        for(int i=0; i< this.sentences.length; i++) {
+            items[i] = new Pair(this.sentences[i], false);
+        }
+
+        while (result.size() < num) {
+            int i = this.random.nextInt(this.sentences.length);
+            Pair pair = items[i];
+
+            if (!pair.value) {
+                pair.value = true;
+                result.add(pair.key); 
+            }
+        }
+        return result;
+    }
+}
+
+class Pair {
+    public String key;
+    public Boolean value;
+
+    public Pair(String key, Boolean value) {
+        this.key = key;
+        this.value = value;
     }
 }
